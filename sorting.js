f3e18d20e55504725889513b9e4fa5c2343362f6
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-07 15:51:28 +0000

    Added initial code and comments for what function needs to do

---
+++ sorting.js
@@ -0,0 +1,10 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+//Sort in alphabetical order
+//If students have same name, sort descending by age
+
